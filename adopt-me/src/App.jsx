6c0a538baf63cec40f358e0324941f5af4255716
Ryan Van Valkenburg <ryanvanvalkenburg@gamestop.com>
2023-03-04 13:57:46 -0800

    Add ESLint config, update scripts, create Pet and SearchParams components, and add style.css

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,34 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+const App = () => (
+  <div>
+    <h1>Adopt Me!</h1>
+    <SearchParams />
+  </div>
+);
 
-const App = () => {
-  return React.createElement("div", null, [
-    React.createElement("h1", null, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-  ]);
-};
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const root = createRoot(container);
+root.render(<App />);
